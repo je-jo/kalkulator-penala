b25b99469a232da87a0cc14b4f57c263ef3f6eaa
Jelena Jovicic <jejovicic@gmail.com>
2022-04-16 20:14:39 +0200

    Star current bp selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,49 +1,77 @@
-
-let startDay = 0;
-let startMonth = 0;
-let startYear = 0;
-const inputStartDate = document.querySelector("#input-start-date");
-const inputStartMonth = document.querySelector("#input-start-month");
-const inputStartYear = document.querySelector("#input-start-year");
+let startDay, startMonth, startYear;
+let currentDay, currentMonth, currentYear;
+let totalDaysPassed, totalDaysLeft;
+const dateInput = document.querySelector("#contract-start-date");
 const buttonDate = document.querySelector("#btn--date");
+const displayTimePassed = document.querySelector("#time-passed");
+const displayTimeLeft = document.querySelector("#time-left");
+
+function setTodaysDate() {
+    const displayToday = document.querySelector("#today");
+    currentDay = new Date().getDate();
+    currentMonth = new Date().getMonth() + 1;
+    currentYear = new Date().getFullYear();
+    let today = `${new Date().getDate()}.${currentMonth}.${currentYear}.`
+    displayToday.textContent = `Danasnji datum: ${today}`;
+}
 
 function setStartDate() {
-    if (inputStartDate.value == 31) {
-        startDay = 30
+    let startDate = dateInput.value.split("-");
+    startYear = startDate[0]; //assumes date format '2021', '12', '31'
+    startMonth = startDate[1];
+    if (startDate[2] == 31) {
+        startDay = 30;
     } else {
-        startDay = +inputStartDate.value;
+        startDay = startDate[2];
     }
-    startMonth = +inputStartMonth.value;
-    startYear = +inputStartYear.value;
+}
+
+function calculateTotalDays() {
+    totalDaysPassed = ((30 - startDay) + ((12 - startMonth) * 30) + currentDay + ((currentMonth - 1) * 30));
+    if ((currentYear - startYear) === 2) {
+        totalDaysPassed += 360;
+    }
+    totalDaysLeft = 719 - totalDaysPassed;
+    let formattedDaysPassed = `${Math.floor(totalDaysPassed / 30)} M i ${totalDaysPassed % 30} D`;
+    displayTimePassed.textContent = `Proteklo vreme: ${formattedDaysPassed}`;
+    let formattedDaysLeft = `${Math.floor(totalDaysLeft / 30)} M i ${totalDaysLeft % 30} D`;
+    displayTimeLeft.textContent = `Preostalo vreme: ${formattedDaysLeft}`;
 }
 
 buttonDate.addEventListener("click", () => {
     setStartDate();
-    let totalDaysPassed = ((30 - startDay) + ((12 - startMonth) * 30) + currentDay + ((currentMonth - 1) * 30));
-    if ((currentYear - startYear) === 2) {
-        totalDaysPassed += 360;
+    calculateTotalDays();
+});
+
+const currentBillingProduct = document.querySelector("#current-bp");
+currentBillingProduct.addEventListener("change", (e) => {
+    billingProducts.forEach((product) => {
+        if (product.name === e.currentTarget.value) {
+            console.log(product.price)
+        }
+    })
+});
+
+const billingProducts = [
+    {
+        name: "silver",
+        price: 1500
+    },
+    {
+        name: "gold",
+        price: 2500
+    },
+    {
+        name: "light",
+        price: 2000
+    },
+    {
+        name: "full",
+        price: 4000
     }
-    let totalDaysLeft = 719 - totalDaysPassed;
-    let formattedDaysPassed = `${Math.floor(totalDaysPassed / 30)} M i ${totalDaysPassed % 30} D`
-    displayTimePassed.textContent += ` ${formattedDaysPassed}`;
-    let formattedDaysLeft = `${Math.floor(totalDaysLeft / 30)} M i ${totalDaysLeft % 30} D`
-    displayTimeLeft.textContent += ` ${formattedDaysLeft}`
-})
+]
 
-
-
-
-const displayToday = document.querySelector("#today");
-let currentDay = new Date().getDate();
-let currentMonth = new Date().getMonth() + 1;
-let currentYear = new Date().getFullYear();
-let today = `${currentDay}.${currentMonth}.${currentYear}.`
-displayToday.textContent += ` ${today}`;
-
-const displayTimePassed = document.querySelector("#time-passed");
-
-
-const displayTimeLeft = document.querySelector("#time-left");
+setTodaysDate();
 
 
 
